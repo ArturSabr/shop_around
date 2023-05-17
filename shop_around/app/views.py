ec20902eeb3499f14d8cfb2b162cf900fb84e4a3
gazsuv2 <Narmi.1045@yandex.ru>
2023-05-17 14:34:33 +0300

    refactor

--- shop_around/app/views.py
+++ shop_around/app/views.py
@@ -7,5 +7,16 @@ from .models import *
 class HomeView(ListView):
     model = Product
     template_name = 'index.html'
+    queryset = Product.objects.all()
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['products_4'] = Product.objects.all()[-4:]
+        context['pols'] = Pol.objects.all()
+        context['sizes'] = Size.objects.all()
+        context['galleries'] = Gallery.objects.all()
+        return context
     class Meta:
         pass
